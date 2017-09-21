380ce31533f46b165dcbc6975048d4f800d66407
stevengalvis <steven.galvis@gmail.com>
2017-09-20 23:46:42 -0400

    added inputs for the guess input

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default function guessForm(props) {
+export default function GuessForm(props) {
   return (
-    
+    <div className="form-group">
+      <input type="number" name="userGuess" id="userGuess" placeHolder="Enter your guess" required/>
+      <input type="submit" id="guessButton" name="submit" value="Guess"
+      </div>
+
   )
 }
